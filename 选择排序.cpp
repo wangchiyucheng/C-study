8ddde136014766821490a5480ae75babb4ccfe6c
wangchiyucheng <1132209031@qq.com>
2019-04-11 15:06:18 +0800

    C

---
+++ 选择排序.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void swap(int *a,int *b);
+void selection_sort(int arr[], int len);
+int main() 
+{
+    int arr[] = { 22, 34, 3, 32, 82};
+    int len = (int) sizeof(arr) / sizeof(*arr);
+    selection_sort(arr, len);
+    int i;
+    for (i = 0; i < len; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
+void swap(int *a,int *b) //交換兩個變數
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void selection_sort(int arr[], int len) 
+{
+    int i,j;
+ 
+    for (i = 0 ; i < len - 1 ; i++) 
+    {
+        int min = i;
+        for (j = i + 1; j < len; j++)     //走訪未排序的元素
+            if (arr[j] < arr[min])    //找到目前最小值
+                min = j;    //紀錄最小值
+           swap(&arr[min], &arr[i]);    //做交換
+       }
+}
